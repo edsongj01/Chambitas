9fcd56d3d0d1db48f1ac5f3e4171de5600577268
edsongj01 <edson2000.nosde@gmail.com>
2021-11-19 22:18:06 -0600

    VF 2.02 Corr: Diseño 99%

--- app/src/main/java/com/pds/chambitas/ui/configuracion/ConfiguracionFragment.kt
+++ app/src/main/java/com/pds/chambitas/ui/configuracion/ConfiguracionFragment.kt
@@ -1,10 +1,23 @@
 package com.pds.chambitas.ui.configuracion
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -13,9 +26,15 @@ import com.pds.chambitas.R
 import kotlinx.android.synthetic.main.fragment_aceptacionservicio.view.*
 import kotlinx.android.synthetic.main.fragment_configuracion.*
 import kotlinx.android.synthetic.main.fragment_configuracion.view.*
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+
+
 
 
 class ConfiguracionFragment : Fragment() {
+
+    var cambiofoto: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,13 +43,99 @@ class ConfiguracionFragment : Fragment() {
 
         val root: View = inflater.inflate(R.layout.fragment_configuracion, container, false)
 
+        cambiofoto = root.findViewById(R.id.btneditarfoto);
+
         val cambiocontra = Navigation.createNavigateOnClickListener(R.id.action_nav_configuracion_to_cambiocontraFragment)
         root.btnCambiocontra.setOnClickListener {
             cambiocontra.onClick(it)
         }
 
+        cambiofoto?.setOnClickListener {
+            requestPermission()
+            //dispatchTakePictureIntent()
+        }
+
 
         return root
+    }
+
+    //CAMARA
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(
+            intent,
+            REQUEST_IMAGE_CAPTURE
+        )
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            takePictureIntent.resolveActivity(activity().getpackageManager)?.also {
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//            }
+//        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView16.setImageBitmap(imageBitmap)
+            println("ESTA ES LA IMAGEN  "+imageBitmap)
+        }
+    }
+
+    // GALERIA
+    private fun requestPermission() {
+        // Verificaremos el nivel de API para solicitar los permisos
+        // en tiempo de ejecución
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    pickPhotoFromGallery()
+                }
+
+                else -> requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else {
+            // Se llamará a la función para APIs 22 o inferior
+            // Esto debido a que se aceptaron los permisos
+            // al momento de instalar la aplicación
+            pickPhotoFromGallery()
+        }
+    }
+
+    private fun pickPhotoFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startForActivityResult.launch(intent)
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){ isGranted ->
+
+        if (isGranted){
+            pickPhotoFromGallery()
+        }else{
+            Toast.makeText(
+                context,
+                "Permission denied",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val startForActivityResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            val data = result.data?.data
+            imageView16.setImageURI(data)
+        }
     }
 
 }
