37eb0d5ff18395d1e7d808a6503f7de8ba696d3b
edsongj01 <edson2000.nosde@gmail.com>
2021-11-21 22:40:32 -0600

    VF 2.03 Corr: Diseño 99%

--- app/src/main/java/com/pds/chambitas/ui/configuracion/ConfiguracionFragment.kt
+++ app/src/main/java/com/pds/chambitas/ui/configuracion/ConfiguracionFragment.kt
@@ -1,6 +1,7 @@
 package com.pds.chambitas.ui.configuracion
 
 import android.Manifest
+import android.R.attr
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -11,26 +12,23 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import androidx.navigation.Navigation
 import com.pds.chambitas.R
 import kotlinx.android.synthetic.main.fragment_aceptacionservicio.view.*
 import kotlinx.android.synthetic.main.fragment_configuracion.*
 import kotlinx.android.synthetic.main.fragment_configuracion.view.*
 import androidx.core.app.ActivityCompat.startActivityForResult
+import android.graphics.BitmapFactory
 
-
-
-
+import android.R.attr.data
+import android.widget.LinearLayout
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ConfiguracionFragment : Fragment() {
 
@@ -45,14 +43,37 @@ class ConfiguracionFragment : Fragment() {
 
         cambiofoto = root.findViewById(R.id.btneditarfoto);
 
-        val cambiocontra = Navigation.createNavigateOnClickListener(R.id.action_nav_configuracion_to_cambiocontraFragment)
+        val cambiocontra =
+            Navigation.createNavigateOnClickListener(R.id.action_nav_configuracion_to_cambiocontraFragment)
         root.btnCambiocontra.setOnClickListener {
             cambiocontra.onClick(it)
         }
 
         cambiofoto?.setOnClickListener {
-            requestPermission()
+            //requestPermission()
             //dispatchTakePictureIntent()
+
+            val bottomSheetDialog = BottomSheetDialog(
+                requireContext(), R.style.BottomSheetDialogTheme
+            )
+            val bottomSheetView: View = LayoutInflater.from(context)
+                .inflate(
+                    R.layout.dialog_foto,
+                    root.findViewById<View>(R.id.contenedor_cambiofoto) as LinearLayout?
+                )
+            bottomSheetView.findViewById<View>(R.id.btnCamara).setOnClickListener {
+                dispatchTakePictureIntent()
+                bottomSheetDialog.dismiss()
+            }
+            bottomSheetView.findViewById<View>(R.id.btnAlbum).setOnClickListener {
+                requestPermission()
+                bottomSheetDialog.dismiss()
+            }
+            bottomSheetView.findViewById<View>(R.id.btnCancelar).setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
         }
 
 
@@ -69,11 +90,6 @@ class ConfiguracionFragment : Fragment() {
             intent,
             REQUEST_IMAGE_CAPTURE
         )
-//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-//            takePictureIntent.resolveActivity(activity().getpackageManager)?.also {
-//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-//            }
-//        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -81,14 +97,12 @@ class ConfiguracionFragment : Fragment() {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             imageView16.setImageBitmap(imageBitmap)
-            println("ESTA ES LA IMAGEN  "+imageBitmap)
+            println("ESTA ES LA IMAGEN  " + imageBitmap)
         }
     }
 
     // GALERIA
     private fun requestPermission() {
-        // Verificaremos el nivel de API para solicitar los permisos
-        // en tiempo de ejecución
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             when {
 
@@ -101,10 +115,7 @@ class ConfiguracionFragment : Fragment() {
 
                 else -> requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
             }
-        }else {
-            // Se llamará a la función para APIs 22 o inferior
-            // Esto debido a que se aceptaron los permisos
-            // al momento de instalar la aplicación
+        } else {
             pickPhotoFromGallery()
         }
     }
@@ -117,22 +128,23 @@ class ConfiguracionFragment : Fragment() {
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
-    ){ isGranted ->
+    ) { isGranted ->
 
-        if (isGranted){
+        if (isGranted) {
             pickPhotoFromGallery()
-        }else{
+        } else {
             Toast.makeText(
                 context,
                 "Permission denied",
-                Toast.LENGTH_SHORT).show()
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
     private val startForActivityResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ){ result ->
-        if (result.resultCode == Activity.RESULT_OK){
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
             val data = result.data?.data
             imageView16.setImageURI(data)
         }
